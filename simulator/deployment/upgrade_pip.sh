14ab6921735082c71e0a2e557fff52e8b9b1d5b7
Meng Wang <mengwanguc@gmail.com>
2023-09-26 03:41:11 +0000

    fix for python 3.8 and ubuntu20

--- simulator/deployment/upgrade_pip.sh
+++ simulator/deployment/upgrade_pip.sh
@@ -8,5 +8,7 @@ python -m pip install --upgrade setuptools
 pip install --no-cache-dir  grpcio 
 pip install --no-cache-dir  grpcio-tools 
 pip install --no-cache-dir  python-numa
-apt install python3.6-dev
+sudo apt install python3.8-dev
 python -m pip install psutil 
+
+sudo apt-get install libnuma-dev  # this is required for "pip install numa"
